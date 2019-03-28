575ca78e85fa2caec48951ce0187b33bdb462f60
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-28 12:27:57 -0500

    got rid of or 4000 in port const

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 
-const port = process.env.PORT || 4000 
+const port = process.env.PORT 
 
 // const greeting = process.env.GREETING
 
